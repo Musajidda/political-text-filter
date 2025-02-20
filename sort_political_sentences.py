07309044066d9ebfea2052ddd544a82633711b67
Musajidda <muhammadmjidder8@gmail.com>
2025-02-20 12:47:48 +0100

    updated

---
+++ sort_political_sentences.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+
+df = pd.read_csv('political_sentences3.csv')
+
+
+df['label'] = df['label'].str.lower()
+
+
+output_dirs = ['Positive', 'Negative', 'Neutral']
+
+for folder in output_dirs:
+    os.makedirs(folder, exist_ok=True)
+
+
+for label in output_dirs:
+    filtered_df = df[df['label'] == label.lower()]
+    filtered_df.to_csv(f'{label}/{label}_sentences.csv', index=False)
+
+print("Sentences sorted into Positive, Negative, and Neutral folders successfully.")
